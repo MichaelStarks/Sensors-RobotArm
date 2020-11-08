d8d72d5ae01a43fa4e78bb770714a9cb02f00701
Caroline <carolinelassiter@gmail.com>
2020-11-08 09:04:23 -0500

    Fixed wrist not normal to x axis

--- Robot_Arm_no_threads.py
+++ Robot_Arm_no_threads.py
@@ -19,17 +19,15 @@ class RobotArm:
         self.WRIST = 6.10236
         for i in range(1,9):
                 self.motors.append(self.robot_arm_dxl.new_ax12(i))
-                self.motors[-1].set_position_mode(goal_current=650)
+                self.motors[-1].set_position_mode(goal_current=700)
                 self.motors[-1].torque_enable()
         self.calibrate()
         self.set_base(0)
         time.sleep(.5)
         self.move(5,4)
-        time.sleep(.5)
         for motor in self.motors:
             motor.set_position_mode(goal_current=300)
         self.hand_close()
-        self.set_shoulder(45)
 
     def __del__(self):
         for motor in self.motors:
@@ -93,24 +91,30 @@ class RobotArm:
         self.set_shoulder(150)
         self.set_wrist_vertical(150)
 
+    def ___sync_motors__(self):
+        self.motors[4].set_angle(self.motors[4].get_angle())
+        self.motors[2].set_angle(self.motors[2].get_angle())
+
     def move(self,x,z,phi=180):
         z = z + 1.75
         a = np.sqrt(self.BASE_HEIGHT**2 + x**2)
         alpha = np.tan(x/self.BASE_HEIGHT)
         c = np.sqrt(x**2 + (self.WRIST+(z-self.BASE_HEIGHT))**2)
         omega = np.arccos((c**2 + x** 2 - (self.WRIST + (z-self.BASE_HEIGHT))**2)/(2.0*c*x))
-        theta_2 = np.arccos((self.UPPER_ARM**2 + self.FOREARM**2 - c**2)/(2.0*self.UPPER_ARM*self.FOREARM)) - np.deg2rad(26)
+        theta_2 = np.arccos((self.UPPER_ARM**2 + self.FOREARM**2 - c**2)/(2.0*self.UPPER_ARM*self.FOREARM)) - np.deg2rad(16)
         epsilon = np.arccos((c**2+self.UPPER_ARM**2-self.FOREARM**2)/(2.0*c*self.UPPER_ARM))
-        theta_1 = np.pi - (epsilon + omega) - np.deg2rad(20)
+        theta_1 = np.pi - (epsilon + omega) - np.deg2rad(10)
         theta_3 = np.deg2rad(phi) - ((epsilon + omega) + theta_2)
         theta_1 = np.degrees(theta_1)
         theta_2 = np.degrees(theta_2)
-        # print("Shoulder: " + str(theta_1))
-        # print("Elbow: " + str(theta_2))
-        # print("Wrist: " + str(theta_3))
+        print("Shoulder: " + str(theta_1))
+        print("Elbow: " + str(theta_2))
+        print("Wrist: " + str(theta_3))
         self.set_elbow(theta_2)
         self.set_shoulder(theta_1)
+        if theta_3 < 0:
+            theta_3 = 0
         self.set_wrist_vertical(theta_3,radians=True)
-        # self.current_x = x
-        # self.current_z = z
+        time.sleep(2)
+        self.___sync_motors__()
         return True
