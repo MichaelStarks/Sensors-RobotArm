0e731de654890e1218be3aeeb8d0a374b6bfbbe1
Michael Starks <starksmichael224@gmail.com>
2020-11-12 22:05:37 -0500

    Created Function to get distance

--- IR-Sensor/IR_Sensor.py
+++ IR-Sensor/IR_Sensor.py
@@ -4,8 +4,6 @@ import VL53L0X
 tof = VL53L0X.VL53L0X()
 tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
 
-while True:
-    D = tof.get_distance()
 
-    print(D)
-    time.sleep(1)
+def get_distance():
+    return tof.get_distance()
